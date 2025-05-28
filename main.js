5059091b8be7e5eaac9306e3768521490dd73cff
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-05-28 23:37:34 +0200

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const Row = 7;
+for (let i = 1; i <= Row; i++){
+    let pattern = '';
+    for (j = 1; j <= i ; j++) {
+        pattern+= "*"; 
+    }
+    console.log(pattern);
+}
