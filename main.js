22358fd9d536419d8d17af25aaef2c60305a52a0
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-01 13:49:39 +0200

    datatype commit

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-let favoriteAnimal = 'ANIMAL';
-console.log('My favorite animal:' + favoriteAnimal);
+let newVariable = true;
+console.log(typeof newVariable);
+ newVariable = 61;
+console.log(typeof newVariable);
+newVariable = 'Happy';
+console.log (typeof newVariable);
