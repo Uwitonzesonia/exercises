dd93f293482108d57b28a6820cb855a7838f125f
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-01 18:21:31 +0200

    kelvin3 commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const kelvin = 0;// kevin will not be changed , const kelvin = 293
+const kelvin = 0;// kevin will not be changed , const kelvin = 293Add commentMore actions
 const celsius = kelvin - 273 ;//  celsius will not changed . const celsius = kelvin - 273 ;
  let Fahrenheit = celsius * (9 / 5) + 32; // fahrenheit can be changed . let fahrenheit = celsius * (9 / 5) + 32 ;
 Fahrenheit = Math.floor(Fahrenheit);// fahrenheit will come in decimal numbers
