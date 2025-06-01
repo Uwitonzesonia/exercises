26b75643790bb42a4cbc21e5b67a031db47dea92
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-01 21:54:31 +0200

    multiply error commit

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-function multiply(number) {
-let numberOfDigits;
-  if (number === 0) {
-    numberOfDigits = 1;
-  } else {
-    numberOfDigits = String(Math.abs(number)).length;
-  }
-  const fiveToPowerOfDigits = Math.pow(5, numberOfDigits);
-  const result = number * fiveToPowerOfDigits ;
-  return result;
+ function  multiply(a, b) {
+  const multiply = a * b;
+  return multiply;
 }
+
+
