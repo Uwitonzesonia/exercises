fe6b340c9916e14eed6f3fb3f7633dba0b7ff74a
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-01 18:33:51 +0200

    multiply commit

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const kelvin = 0;// kevin will not be changed , const kelvin = 293Add commentMore actions
-const celsius = kelvin - 273 ;//  celsius will not changed . const celsius = kelvin - 273 ;
- let Fahrenheit = celsius * (9 / 5) + 32; // fahrenheit can be changed . let fahrenheit = celsius * (9 / 5) + 32 ;
-Fahrenheit = Math.floor(Fahrenheit);// fahrenheit will come in decimal numbers
-console.log (`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
-let newton = celsius * (33/100);
-newton = Math.floor(newton);
-console.log(`The temperature is ${newton} degrees Newton.`);
+function multiply(number) {
+  const digits = number.toString().length;
+  const multiplier = Math.pow(5, digits);
+  return number * multiplier;
+}
+console.log(multiply(2));
+console.log(multiply(12));
+console.log(multiply(123));
+console.log(multiply(9876));
