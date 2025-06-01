c2f151c732d4b5f5a73d8d143e99d909f24948f1
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-01 16:15:03 +0200

    kelvin commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let newVariable = true;
-console.log(typeof newVariable);
- newVariable = 61;
-console.log(typeof newVariable);
-newVariable = 'Happy';
-console.log (typeof newVariable);
+const kelvin = 0;// kevin will not be changed , const kelvin = 293
+const celsius = kelvin - 273 ;//  celsius will not changed . const celsius = kelvin - 273 ;
+ let Fahrenheit = celsius * (9 / 5) + 32; // fahrenheit can be changed . let fahrenheit = celsius * (9 / 5) + 32 ;
+Fahrenheit = Math.floor(Fahrenheit);// fahrenheit will come in decimal numbers
+console.log (`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
