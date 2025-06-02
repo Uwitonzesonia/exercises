8ed64b712ecca9307a645e47584330ee4119ec67
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-02 23:42:12 +0200

    converting age commit

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function sayHello( name, city, state ) {
-  const fullname = name.join(' ');
-  return` Hello, ${fullname}! welcome to ${city}, ${state}!`;
-}
+const myAge = 19 ;// I declare myage const myAge = 19 
+let earlyYears = 2; // I declare variable earlyYears which is equal to the two
+earlyYears *= 10.5 ; 
+let laterYears = myAge - 2; 
+laterYears *= 4 ; 
+console.log(earlyYears);
+console.log(laterYears);
+const myAgeInDogYears = earlyYears + laterYears ;
+ let myName = 'dog'.toLowerCase(); 
+ console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in human years which is DOG AGE years old in dog years.`);
 
 
