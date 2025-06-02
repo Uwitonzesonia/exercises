7a7d6ea05fe59cae7741039201d63d7d1cb5a152
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-03 00:28:24 +0200

    if condition commit

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const myAge = 19 ;// I declare myage const myAge = 19 
-let earlyYears = 2; // I declare variable earlyYears which is equal to the two
-earlyYears *= 10.5 ; 
-let laterYears = myAge - 2; 
-laterYears *= 4 ; 
-console.log(earlyYears);
-console.log(laterYears);
-const myAgeInDogYears = earlyYears + laterYears ;
- let myName = 'dog'.toLowerCase(); 
- console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in human years which is DOG AGE years old in dog years.`);
-
-
+let sale = true ;
+sale = false ;
+if (sale) {
+  console.log('Time to buy!');
+} 
