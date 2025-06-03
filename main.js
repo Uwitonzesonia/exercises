986004c889ac84f75dc3f45d3e3b7a28ac86f63d
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-04 00:40:36 +0200

    append array commit

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-let arr = ['first', 'second', 'third'];
-let first = arr[0];
-let second = arr[1];
-let last = arr[arr.length -1];
-console.log(first);
-console.log(second);
-console.log(last);
+var a = ['first', 'second'];
+a.push('third');
+console.log(a);
