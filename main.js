1b356d8f0a3f659268f5f8dae04e5ded347fe847
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-07 16:23:24 +0200

    array commit

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let favoritePhrase = 0;
-if (favoritePhrase) {
-  console.log('this string is definitely complete');
-} else {
-console.log('this string is definitely empty');
-}
+var a = ['first', 'second'];
+a.push('third');
+console.log('a:', a);
