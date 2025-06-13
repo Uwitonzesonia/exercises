c3dcaac5927615f57dea0c40090b5360e5bfd334
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-06-14 00:19:26 +0200

    average loop commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function betterThanAverage(classPoints, yourPoints) {
+  let total = 0;
+  for (let i = 0; i < classPoints.length; i++) {
+    total += classPoints[i];
+  }
+  const average = total / classPoints.length;
+  return yourPoints > average;
+}
+console.log(betterThanAverage([40, 60, 70, 80, 90, 67], 85));
+console.log(betterThanAverage([45, 65, 67, 87, 98, 89], 90));
